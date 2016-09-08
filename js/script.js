09394c4f3ba2e65ced3ad469d4aa24fae5270192
Deep Sethi <deeptanshu.146@gmail.com>
2016-09-07 19:19:22 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+  $('.event_header').click(function() {
+    var id = "#event"+$(this).attr("id");
+    $(id).slideToggle(120);
+  });
+  var team_height = $('.team_bar').height();
+  var team_width = $('.team_bar').width();
+  if(team_height != undefined && team_width != undefined) {
+    team_height+=10;
+    team_height += "px";
+    team_width += "px";
+    $('#team_img').css({
+      'height': team_height,
+      'width': team_width
+    });
+  }
+  $('.post_minified').click(function() {
+    var post = $(this);
+    $('#blog_expand').show();
+    $('#content_blog').slideToggle(120);
+    var profile = post.find('img.row_1').attr("src");
+    $('.poster').attr('src',profile);
+    var title = post.find('.post_title').text();
+    $('.blog_title').text(title);
+    var img_main = post.find('.img_preview').attr("src");
+    $('.preview_expand').attr('src', img_main);
+    var dim = (($("#content_blog").width() - $('.preview_expand').width())/2)+"px";
+    $('.preview_expand').css({
+      'margin-left': dim
+    });
+    $('.post_full').text($('.post_text_full').text());
+    $('.blog_date').text(post.find('.date').text());
+  });
+
+
+  $('.cl:not(#content_blog)').click(function() {
+    $('#blog_expand').hide();
+    $('#content_blog').slideToggle(120);
+  });
+  $(document).keyup(function(event) {
+    if(event.which == 27) {
+      if($('#blog_expand').css('display') == 'block') {
+        $('#blog_expand').hide();
+        $('#content_blog').slideToggle(120);
+      }
+    }
+  });
+});
